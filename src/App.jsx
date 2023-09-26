2b6b337daed1991dfe190578da48dfad8aa9be90
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-26 13:45:00 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import config from './config/config'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+const config = {
+  name: String(import.meta.env.VITE_NAME)
+}
+
+export default config
