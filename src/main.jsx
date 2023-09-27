5351edb3d808f2d5f13de3dab72348497fc020a9
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-27 14:49:48 +0530

    Added Footer component

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+      
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 )
