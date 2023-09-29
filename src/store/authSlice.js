9f75a45320453720dbfbad4361585bddab407e3f
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-29 12:23:07 +0530

    Minor change in authentication reducer

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const userToken = localStorage.getItem('token')
+
 const initialState = {
-  token: localStorage.getItem('token'),
-  isLoggedin: false,
+  token: userToken,
+  isLoggedin: userToken ? true : false,
   userData: {}
 }
 
