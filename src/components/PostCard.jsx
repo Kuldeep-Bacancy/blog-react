2b73f7a39e8f07423d413a69359761308b069c3c
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-10-04 10:28:43 +0530

    Change post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,15 +4,11 @@ import { Link } from 'react-router-dom'
 function PostCard({id, attributes}) {
   return (
     <Link to={`/article/${id}`}>
-      <div className='w-full bg-gray-100 rounded-xl p-4'>
-        <div className='w-full justify-center mb-4'>
-          <img src={attributes.image || "https://images.unsplash.com/photo-1586943759341-be5595944989?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MDE1MDB8MHwxfHNlYXJjaHwyNHx8YXJ0aWNsZXxlbnwwfHx8fDE2OTYzOTQxNTN8MA&ixlib=rb-4.0.3&q=85"} alt={attributes.title}
-            className='rounded-xl' />
-
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <img className="rounded-t-lg" src={attributes.image || "https://images.unsplash.com/photo-1586943759341-be5595944989?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MDE1MDB8MHwxfHNlYXJjaHwyNHx8YXJ0aWNsZXxlbnwwfHx8fDE2OTYzOTQxNTN8MA&ixlib=rb-4.0.3&q=85"} alt={attributes.title} />
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{attributes.title}</h5>
         </div>
-        <h2
-          className='text-xl font-bold'
-        >{attributes.title}</h2>
       </div>
     </Link>
   )
